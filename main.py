a745585af0289d9bf9a4ca0f1ba1c69346e74756
sommaysingh0-coder <sommaysingh0@gmail.com>
2026-06-12 21:48:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ if st.button("Try AI"):
     st.text("for running the code, make sure you have installed python ")
     st.text("open terminal and run the command: python ")
     st.text("its looks like this :-")
-    st.image("/home/sommay/Pictures/s/Screenshot_2026-06-12_19-31-48.png")
+    st.image("Screenshot_2026-06-12_19-31-48.png")
     st.text("if it's not look like this means your python was not installed .")
     st.text("second step is enter this command in terminal:-")
-    st.image("/home/sommay/Pictures/s/Screenshot_2026-06-12_19-49-38.png")
+    st.image("Screenshot_2026-06-12_19-49-38.png")
     st.text("the last step is emter this command in terminal:-")
     with st.expander("Show Code"):
-      with open("/home/sommay/Desktop/code/TipsAI/tips.py", "r") as f:
+      with open("tips.py", "r") as f:
          content = f.read()
          st.code(content, language="python")
