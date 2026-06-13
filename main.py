1a66eeb954400674949088f2bc3dd1e823a32b53
sommaysingh0-coder <sommaysingh0@gmail.com>
2026-06-13 19:39:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.html('<meta name="google-site-verification" content="QtsAa4zSDNnVR1bOmdh4CEce4qUvFPlsH87nWCo4QLc" />" />')
+
 st.title("I AM SOMMAY and this is my first AI project")
 st.text("If you want to try the AI, press the button below.")
 if st.button("Try AI"):
@@ -17,3 +17,4 @@ if st.button("Try AI"):
       with open("tips.py", "r") as f:
          content = f.read()
          st.code(content, language="python")
+         st.html('<meta name="google-site-verification" content="QtsAa4zSDNnVR1bOmdh4CEce4qUvFPlsH87nWCo4QLc" /> " />')
